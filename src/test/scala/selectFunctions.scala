a73950490666addd83ff9faf8e082362fcb38d0a
Miguel A Iglesias <miguel@human.x.ai>
2017-02-17 10:24:30 -0500

    can make a Seq too

--- src/test/scala/selectFunctions.scala
+++ src/test/scala/selectFunctions.scala
@@ -32,7 +32,9 @@ class SelectFunctionsTests extends FunSuite with Matchers {
   //is possible to have generators with the same context, they will all be used
   val featureGenerator3_1 = (x: String, s: Char, i: Int) => s.toInt + i * 2 + x.size
 
-  val featureGenerator4 = (x: String) => "string_size" -> x.size
+  val featureGenerator4 = (x: String) => x.size
+
+  val featureGenerator5 = (x: Char) => x.toInt
 
   object FeatureGenerators {
 
@@ -41,35 +43,43 @@ class SelectFunctionsTests extends FunSuite with Matchers {
         featureGenerator2 ::
         featureGenerator3 ::
         featureGenerator3_1 ::
-        featureGenerator4 :: HNil
+        featureGenerator4 ::
+        featureGenerator5 :: HNil
   }
 
   val hi = "hi"
   test("single argument") {
     assert(
-      SelectFunctions.runAll(hi)(FeatureGenerators.generators) == "string_size" -> 2 :: HNil
+      SelectFunctions.runAll(hi)(FeatureGenerators.generators) == 2 :: HNil
     )
   }
   test("two arguments") {
     assert(
-      SelectFunctions.runAll(hi, 1)(FeatureGenerators.generators) == "hi + 1" :: "string_size" -> 2 :: HNil
+      SelectFunctions.runAll(hi, 1)(FeatureGenerators.generators) == "hi + 1" :: 2 :: HNil
     )
   }
   test("three arguments") {
     assert(
-      SelectFunctions.runAll(hi, 1, 2d)(FeatureGenerators.generators) == 2 :: "hi + 1" :: "string_size" -> 2 :: HNil
+      SelectFunctions.runAll(hi, 1, 2d)(FeatureGenerators.generators) == 2 :: "hi + 1" :: 2 :: HNil
     )
   }
   test("different three arguments") {
     assert(
-      SelectFunctions.runAll(hi, 'a', 1)(FeatureGenerators.generators) == "hi + 1" :: 1.0 :: 101 :: "string_size" -> 2 :: HNil
+      SelectFunctions.runAll(hi, 'a', 1)(FeatureGenerators.generators) == "hi + 1" :: 1.0 :: 101 :: 2 :: 97 :: HNil
     )
   }
 
   test("four arguments in different order") {
     // the order of the arguments doesn't matter
     assert(
-      SelectFunctions.runAll(hi, 2d, 1, 'a')(FeatureGenerators.generators) === 2 :: "hi + 1" :: 1.0 :: 101 :: "string_size" -> 2 :: HNil
+      SelectFunctions.runAll(hi, 2d, 1, 'a')(FeatureGenerators.generators) === 2 :: "hi + 1" :: 1.0 :: 101 :: 2 :: 97 :: HNil
+    )
+  }
+
+  test("get a Seq") {
+    val res: Seq[Int] = SelectFunctions.runAll(hi, 'a')(FeatureGenerators.generators).runtimeList.map(_.asInstanceOf[Int])
+    assert(
+      res == Seq(2, 97)
     )
   }
 
