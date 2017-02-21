d23ad7c5701eedf8dea0dc5d7adebca653419041
benholtzmansmith <benjamin.holtzman.smith@gmail.com>
2017-02-21 18:07:36 -0500

    adding intersection for coproducts

--- src/main/scala/coproduct.scala
+++ src/main/scala/coproduct.scala
@@ -2,34 +2,37 @@ package ai.x
 package typeless
 
 import shapeless._
+import shapeless.ops.coproduct.Inject
 
 package object coproduct {
-  trait Intersect[L <: Coproduct, +S <:  Coproduct] {
-    type Out
-    def intersect( l: L ): Option[Out]
+  trait Intersect[L <: Coproduct, S <: Coproduct] {
+    def intersect(l: L): Option[S]
   }
 
-  object Intersect {
-    def intersect[L <: Coproduct, S <: Coproduct]( implicit f: Intersect[L, S] ) = f
+  trait Intersect0 {
+    implicit def cCons0[H, T <: Coproduct, S <: Coproduct](
+      implicit
+      subset: Intersect[T, S]
+    ): Intersect[:+:[H, T], S] = new Intersect[H :+: T, S] {
+      def intersect(l: H :+: T): Option[S] = l.tail.flatMap(f => subset.intersect(f))
+    }
+  }
+
+  object Intersect extends Intersect0 {
+    def apply[L <: Coproduct, S <: Coproduct](implicit f: Intersect[L, S]) = f
+    def intersect[L <: Coproduct, S <: Coproduct](implicit f: Intersect[L, S]) = f
     implicit class Ops[L <: Coproduct](l: L) {
-      def intersect[A <: Coproduct](implicit f: Intersect[L, A]):Option[f.Out] = f.intersect(l)
+      def intersect[S <: Coproduct](implicit f: Intersect[L, S]): Option[S] = f.intersect(l)
     }
-    implicit def cCons[L <: Coproduct, H, T <: Coproduct](
-                                                           implicit
-                                                           subset: Intersect[L, T],
-                                                           selector: shapeless.ops.coproduct.Selector[L,H]
-                                                         ) = new Intersect[L, H :+: T] {
-      type Out = :+:[H, subset.Out]
-
-      def intersect( l: L ): Option[:+:[H, subset.Out]] = {
-        l.select[H].map(Inl(_)).orElse(subset.intersect(l).map(Inr(_))):Option[:+:[H, subset.Out]]
-      }
+    implicit def cCons[H, T <: Coproduct, S <: Coproduct](
+      implicit
+      inject: Inject[S, H],
+      subset: Intersect[T, S]
+    ): Intersect[:+:[H, T], S] = new Intersect[H :+: T, S] {
+      def intersect(l: H :+: T): Option[S] = l.head.map(inject(_))
     }
-
-    implicit def cNil[L <: Coproduct]: Intersect[L, Coproduct] = new Intersect[L, CNil] {
-      type Out = CNil
-
-      def intersect( l: L ) = None
+    implicit def cNil[S <: Coproduct]: Intersect[CNil, S] = new Intersect[CNil, S] {
+      def intersect(l: CNil): Option[S] = None
     }
   }
 }
