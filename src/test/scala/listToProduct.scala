6edf696416caf0462e5707b275c950b77a50ad20
benholtzmansmith <benjamin.holtzman.smith@gmail.com>
2017-03-29 10:18:18 -0400

    adding list to product and test

---
+++ src/test/scala/listToProduct.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015-6 x.ai inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.x
+package typeless
+package test
+
+import ai.x.typeless.hlist.ListToProduct
+import org.scalatest.{ FunSuite, Matchers }
+import shapeless._
+
+class listToProduct extends FunSuite with Matchers {
+
+  import ListToProduct.Ops
+
+  sealed trait A
+  case class B() extends A
+  case class C() extends A
+  case class D() extends A
+
+  case class E(b: B, c: C)
+  case class F(b: B, d: D)
+
+  val listA: List[A] = List(B(), C())
+
+  test("can convert a list of values to a product if the values contained within the list match the required types of the product") {
+    assert(listA.toProduct[E] === Some(E(B(), C())))
+  }
+
+  test("won't convert a list, if the types don't match") {
+    assert(listA.toProduct[F] === None)
+  }
+
+}
