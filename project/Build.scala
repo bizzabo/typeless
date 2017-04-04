359d95ed516dbff9505867e90f5f92ace4cc81d4
Miguel A Iglesias <miguel@human.x.ai>
2017-04-04 14:48:47 -0400

    increasing the version to add ListToHList

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object MyBuild extends Build{
   val projectName = "typeless"
   lazy val aRootProject = Project(id = projectName, base = file("."),
     settings = Seq(
-      version := "0.3.0",
+      version := "0.3.1",
       name := projectName,
       scalaVersion := "2.11.8",
       description := "It provides some extra shapeless functionallity",
